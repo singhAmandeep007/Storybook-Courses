784f8afa42fcff6e17932cfbd6432398f34b9fab
aman Si <aman.fsd99@gmail.com>
2023-11-25 16:09:39 +0000

    Created TaskList.jsx and TaskList.stories.jsx to depict different states of TaskList component. Reused TaskStories in TaskListStories. Added decorator

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Task from "./Task";
+
+/**
+ * You might be thinking that the .list-items wrapper is overly simplistic in TaskList component.
+ *  You're right – in most cases, we wouldn’t create a new component just to add a wrapper.
+ * But the real complexity of the TaskList component is revealed in the edge cases withPinnedTasks, loading, and empty as presented in TaskList.stories.jsx
+ */
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items" key={"empty"} data-testid="empty">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <p className="title-message">You have no tasks</p>
+          <p className="subtitle-message">Sit back and relax</p>
+        </div>
+      </div>
+    );
+  }
+
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
+
+  return (
+    <div className="list-items">
+      {tasksInOrder.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+}
+
+TaskList.propTypes = {
+  /** Checks if it's in loading state */
+  loading: PropTypes.bool,
+  /** The list of tasks */
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+};
+TaskList.defaultProps = {
+  loading: false,
+};
