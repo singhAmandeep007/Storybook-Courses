a04aba6023c812c5ec115c855b4bbc909ffad67b
aman Si <aman.fsd99@gmail.com>
2023-11-25 17:15:00 +0000

    Added InboxScreen and its stories. Added fetchTasks thunk to fetch tasks remotely and cases to handle loading, success and error state. Added msw and msw-storybook-addon as a global decorator to mock API services in storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,17 @@
 import "../src/index.css";
 
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
 // Configures Storybook to log the actions( onArchiveTask and onPinTask )
 // in the UI
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  // enable API mocking
+  decorators: [mswDecorator],
   parameters: {
     // configure how the actions (mocked callbacks) are handled.
     // when we build a pin button,
---
+++ src/components/InboxScreen.stories.jsx
@@ -0,0 +1,57 @@
+import InboxScreen from "./InboxScreen";
+import store from "../lib/store";
+
+import { rest } from "msw";
+// reusing stories
+import { MockedState } from "./TaskList.stories";
+
+import { Provider } from "react-redux";
+
+export default {
+  component: InboxScreen,
+  title: "InboxScreen",
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  tags: ["autodocs"],
+};
+
+/**
+ * by doing `export const Error = {}`
+ * We can quickly spot an issue with the error story.
+ * Instead of displaying the right state, it shows a list of tasks.
+ * One way to sidestep this issue would be to provide a mocked version for each state, similar to what we did in the last chapter.
+ * Instead, we'll use a well-known API mocking library msw alongside a Storybook addon to help us solve this issue.
+ */
+
+export const Default = {
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(
+          "https://jsonplaceholder.typicode.com/todos?userId=1",
+          (req, res, ctx) => {
+            return res(ctx.json(MockedState.tasks));
+          }
+        ),
+      ],
+    },
+  },
+};
+
+/**
+ * error story is now working as intended. 
+ * MSW intercepted our remote API call and provided the appropriate response.
+ */
+export const Error = {
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(
+          "https://jsonplaceholder.typicode.com/todos?userId=1",
+          (req, res, ctx) => {
+            return res(ctx.status(403));
+          }
+        ),
+      ],
+    },
+  },
+};
