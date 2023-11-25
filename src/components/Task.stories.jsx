7f09ea9e07b5ba875437d9316eb5c0e384fb5ee0
aman Si <aman.fsd99@gmail.com>
2023-11-25 18:46:41 +0000

    Adding story for edge case in Task component.

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -41,3 +41,15 @@ export const Archived = {
     },
   },
 };
+
+// NOTE: a story that showcases this edge case
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = {
+  args: {
+    task: {
+      ...Default.args.task,
+      title: longTitleString,
+    },
+  },
+};
